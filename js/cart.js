6348b098321ede05fb6689d7f6ca28a74e25337d
zamsyed99 <zameer_bl@yahoo.com>
2025-12-26 19:31:12 +0530

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,7 +1,3 @@
 function addToCart(id) {
-  let cart = JSON.parse(localStorage.getItem("cart")) || [];
-  cart.push(id);
-  localStorage.setItem("cart", JSON.stringify(cart));
-  alert("T-shirt added to cart!");
+  alert("Added product ID: " + id);
 }
-
