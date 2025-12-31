05ddd69519dc49b80270ade9c20603226d936116
zamsyed99 <zameer_bl@yahoo.com>
2025-12-31 15:50:10 +0530

    Update products.js

--- js/products.js
+++ js/products.js
@@ -1,21 +1,21 @@
 const products = [
   {
     id: 1,
-    name: "Classic White T-Shirt Pune City",
+    name: "Classic White T-Shirt",
     price: "₹499",
     image: "./images/tshirts/tshirt1.jpg",
     description: "100% cotton premium quality"
   },
   {
     id: 2,
-    name: "White Oversized T-Shirt Solapur City",
+    name: "White Oversized T-Shirt",
     price: "₹599",
     image: "./images/tshirts/tshirt2.jpg",
     description: "Comfort fit, breathable fabric"
   },
   {
     id: 3,
-    name: "Graphic Streetwear Tee Mumbai City",
+    name: "Graphic Streetwear Tee",
     price: "₹699",
     image: "./images/tshirts/tshirt3.jpg",
     description: "Trendy street style"
