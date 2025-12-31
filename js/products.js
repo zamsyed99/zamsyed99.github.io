351aa9b5c81003d9e50ba4d89e060f3daaf8ec8a
zamsyed99 <zameer_bl@yahoo.com>
2025-12-31 14:26:06 +0530

    Update products.js

--- js/products.js
+++ js/products.js
@@ -2,21 +2,21 @@ const products = [
   {
     id: 1,
     name: "Classic White T-Shirt",
-    price: ₹ 499,
+    price: 499,
     image: "images/tshirts/tshirt1.jpg",
     description: "100% cotton premium quality"
   },
   {
     id: 2,
     name: "White Oversized T-Shirt",
-    price: ₹ 599,
+    price: 599,
     image: "images/tshirts/tshirt2.jpg",
     description: "Comfort fit, breathable fabric"
   },
   {
     id: 3,
     name: "Graphic Streetwear Tee",
-    price: ₹ 699,
+    price: 699,
     image: "images/tshirts/tshirt3.jpg",
     description: "Trendy street style"
   }
