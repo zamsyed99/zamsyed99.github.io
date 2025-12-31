c26b471fbd065380d20777764e53fa5661b01cdc
zamsyed99 <zameer_bl@yahoo.com>
2025-12-31 14:35:31 +0530

    Update products.js

--- js/products.js
+++ js/products.js
@@ -3,22 +3,21 @@ const products = [
     id: 1,
     name: "Classic White T-Shirt",
     price: "₹499",
-    image: "images/tshirts/tshirt1.jpg",
+    image: "./images/tshirts/tshirt1.jpg",
     description: "100% cotton premium quality"
   },
   {
     id: 2,
     name: "White Oversized T-Shirt",
     price: "₹599",
-    image: "images/tshirts/tshirt2.jpg",
+    image: "./images/tshirts/tshirt2.jpg",
     description: "Comfort fit, breathable fabric"
   },
   {
     id: 3,
     name: "Graphic Streetwear Tee",
     price: "₹699",
-    image: "images/tshirts/tshirt3.jpg",
+    image: "./images/tshirts/tshirt3.jpg",
     description: "Trendy street style"
   }
 ];
-
